c7e2a06cc392121b4eab886b0ebec0f87bf7e178
Gregory Wagner <wagner.greg@gmail.com>
2024-02-23 18:38:24 -0600

    updates

--- buoyant_tracer.jl
+++ buoyant_tracer.jl
@@ -4,8 +4,19 @@ using Oceananigans.BoundaryConditions: fill_halo_regions!
 using Printf
 using GLMakie
 
-grid = RectilinearGrid(size=(128, 128, 32), extent=(128, 128, 32))
+Lx = Ly = 128
+Lz = 24
+Nx = Ny = 96
+Nz = 48
 
+# Boundary conditions
+τˣ = -1e-4
+N² = 2e-5
+f = 1e-4
+h₀ = 5 # initial mixed layer depth
+w₀ = 1e-2 # terminal velocity of rising particles
+
+# Waves
 g = 9.81
 a = 0.8    # m
 k = 60     # m
@@ -13,16 +24,16 @@ k = 60     # m
 σ = sqrt(g * k) # s⁻¹
 Uˢ = a^2 * σ * k # m s⁻¹
 
+grid = RectilinearGrid(size=(Nx, Ny, Nz), x=(0, Lx), y=(0, Ly), z=(-Lz, 0))
+
 @inline ∂z_uˢ(z, t, p) = 1 / (2 * p.k) * p.Uˢ * exp(2 * p.k * z)
 stokes_drift = UniformStokesDrift(∂z_uˢ = ∂z_uˢ , parameters=(; k, Uˢ))
 
-Qᵘ = -1e-4
-N² = 2e-5
-u_bcs = FieldBoundaryConditions(top = FluxBoundaryCondition(Qᵘ))
-coriolis = FPlane(f=1e-4) # s⁻¹
+u_bcs = FieldBoundaryConditions(top = FluxBoundaryCondition(τˣ))
+coriolis = FPlane(; f) # s⁻¹
 
 wc = ZFaceField(grid)
-set!(wc, 1e-3)
+set!(wc, w₀)
 fill_halo_regions!(wc)
 c_forcing = AdvectiveForcing(w=wc)
 
@@ -36,29 +47,28 @@ model = NonhydrostaticModel(; grid, coriolis, stokes_drift,
 
 Ξ(z) = randn() * exp(z / 4)
 
-h₀ = 5 # m
-
-bᴺ(z) = if z < - h₀
-    N² * z
-else
+bᴺ(z) = if z > - h₀
     - N² * h₀
+else
+    N² * z
 end
 
 Δz = zspacings(grid, Center())
 
-bᵢ(x, y, z) = bᴺ(z) + 1e-1 * Ξ(z) * N² * model.grid.Lz
-cᵢ(x, y, z) = if z > - Δz
-    rand()
+bᵢ(x, y, z) = bᴺ(z) + 1e-3 * Ξ(z) * N² * model.grid.Lz
+
+cᵢ(x, y, z) = if z > - h₀/2
+    1
 else
     0
 end
 
-u★ = sqrt(abs(Qᵘ))
-uᵢ(x, y, z) = u★ * 1e-1 * Ξ(z)
+u★ = sqrt(abs(τˣ))
+uᵢ(x, y, z) = u★ * 1e-3 * Ξ(z)
 
 set!(model, u=uᵢ, w=uᵢ, b=bᵢ, c=cᵢ)
 
-simulation = Simulation(model, Δt=10.0, stop_time=1hours)
+simulation = Simulation(model, Δt=20.0, stop_iteration=400)
 conjure_time_step_wizard!(simulation, cfl=0.7, max_Δt=1minute)
 
 function progress(simulation)
@@ -83,34 +93,58 @@ Nz = size(grid, 3)
 outputs = merge(model.velocities, model.tracers)
 simulation.output_writers[:xy] = JLD2OutputWriter(model, outputs,
                                                   indices = (:, :, Nz),
-                                                  schedule = TimeInterval(10minutes),
+                                                  schedule = TimeInterval(1minutes),
                                                   filename = "langmuir_particles_xy.jld2",
                                                   overwrite_existing = true)
 
 simulation.output_writers[:yz] = JLD2OutputWriter(model, outputs,
                                                   indices = (1, :, :),
-                                                  schedule = TimeInterval(10minutes),
+                                                  schedule = TimeInterval(1minutes),
                                                   filename = "langmuir_particles_yz.jld2",
                                                   overwrite_existing = true)
+
+averages = (
+    u =  Average(model.velocities.u, dims=(1, 2)),
+    v =  Average(model.velocities.v, dims=(1, 2)),
+    b =  Average(model.tracers.b, dims=(1, 2)),
+    c =  Average(model.tracers.c, dims=(1, 2)),
+)
+            
+simulation.output_writers[:z] = JLD2OutputWriter(model, averages,
+                                                 schedule = TimeInterval(10minutes),
+                                                 filename = "langmuir_particles_averages.jld2",
+                                                 overwrite_existing = true)
+
+
 
 run!(simulation)
 
 wt = FieldTimeSeries("langmuir_particles_xy.jld2", "w")
-ct = FieldTimeSeries("langmuir_particles_xy.jld2", "c")
+cxyt = FieldTimeSeries("langmuir_particles_xy.jld2", "c")
+uyzt = FieldTimeSeries("langmuir_particles_yz.jld2", "u")
+cyzt = FieldTimeSeries("langmuir_particles_yz.jld2", "c")
 Nt = length(wt)
 
 fig = Figure(size=(1200, 600))
 axw = Axis(fig[1, 1])
-axc = Axis(fig[1, 2])
-slider = Slider(fig[2, 1:2], range=1:Nt, startvalue=1)
+axcxy = Axis(fig[1, 2])
+axuyz = Axis(fig[2, 1])
+axcyz = Axis(fig[2, 2])
+slider = Slider(fig[3, 1:2], range=1:Nt, startvalue=1)
 n = slider.value
 wn = @lift interior(wt[$n], :, :, 1)
-cn = @lift interior(ct[$n], :, :, 1)
+cxyn = @lift interior(cxyt[$n], :, :, 1)
+uyzn = @lift interior(uyzt[$n], 1, :, :)
+cyzn = @lift interior(cyzt[$n], 1, :, :)
 
 wlim = 1e-2
-clim = 1e1
+clim = 5e-2
 
 heatmap!(axw, wn, colorrange=(-wlim, wlim), colormap=:balance)
-heatmap!(axc, cn, colorrange=(0, clim), colormap=:solar)
+heatmap!(axcxy, cxyn, colorrange=(0, clim), colormap=:solar)
+
+heatmap!(axuyz, uyzn, colorrange=(-wlim, wlim), colormap=:balance)
+heatmap!(axcyz, cyzn, colorrange=(0, clim), colormap=:solar)
+
 display(fig)
 
